3d1279cd121659bc28b140b4585eaf279d412565
Byran <bemery2@missouriwestern.edu>
2024-10-25 21:45:11 -0500

    update replicator, and add observechange

--- test/client/init.client.lua
+++ test/client/init.client.lua
@@ -3,6 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Replicator = require(ReplicatedStorage.Packages.Replicator).Client
 
 Replicator.ListenToNewReplica("Test", function(Replica)
-    print(Replica)
-    Replica.Changed:Connect(print)
+    Replica:ObserveChange("Friends", function(Friends)
+        print("Friends:", Friends)
+    end)
 end)
