e76da2a71e448947f1d1e8038c2eef7df67e9f44
Byran <byran.emery2@gmail.com>
2024-10-29 14:00:44 -0500

    Updates

--- test/server/init.server.lua
+++ test/server/init.server.lua
@@ -1,17 +1,36 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Replicator = require(ReplicatedStorage.Packages.Replicator).Server
+local Replicator = require(ReplicatedStorage.Packages.Replicator) :: { any }
 
-local Replica = Replicator.NewReplica({
-    Id = "Test",
-    Name = "Test",
-    Tags = { "Test" },
-    Data = {
-        ["Friends"] = {}
-    },
+local Parent = Replicator.NewReplica({
+    Name = "Parent",
     To = "All",
 })
 
-task.delay(5, function()
-    Replica:Set("Friends+", "Siren")
-end)
+local Replica = Replicator.NewReplica({
+    Name = "Test",
+    Tags = { "Test" },
+    Data = {
+        X = 0,
+        Y = 5,
+        Z = 0,
+        Type = "Plastic",
+        T = {
+            A = 1,
+            B = 2,
+            C = 3,
+        }
+    },
+    Parent = Parent,
+})
+
+local Data = Replica.Data
+
+print(Data.T.A)
+Replica:Set("T-", {["A"] = 1})
+print(Data.T.A)
+
+while true do
+    Data.Y += math.random(-10, 10) / 10
+    task.wait()
+end
