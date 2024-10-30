ec0b9f2894e17a62dde749ea1de47ed71e74c95b
Byran <byran.emery2@gmail.com>
2024-10-29 21:38:26 -0500

    add helper functions for arrays

--- test/server/init.server.lua
+++ test/server/init.server.lua
@@ -11,26 +11,15 @@ local Replica = Replicator.NewReplica({
     Name = "Test",
     Tags = { "Test" },
     Data = {
-        X = 0,
-        Y = 5,
-        Z = 0,
-        Type = "Plastic",
-        T = {
-            A = 1,
-            B = 2,
-            C = 3,
-        }
+        TestArray = {},
     },
     Parent = Parent,
 })
 
+Replica:ListenToNewKey("TestArray", function(Key, Value)
+    print("New Key:", Key, "Value:", Value)
+end)
+
 local Data = Replica.Data
 
-print(Data.T.A)
-Replica:Set("T-", {["A"] = 1})
-print(Data.T.A)
-
-while true do
-    Data.Y += math.random(-10, 10) / 10
-    task.wait()
-end
+Data.TestArray:Insert(5)
